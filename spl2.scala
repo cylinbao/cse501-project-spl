0c362d89e9a2fc2ad879ea51528eb6d33d57b86a
Chien-Yu Lin <myislin@gmail.com>
2020-12-18 16:06:16 -0800

    better syntax and codegen

--- spl2.scala
+++ spl2.scala
@@ -6,18 +6,23 @@ object Spl2 {
     case class Ident(name: String) extends Expr {
       override def toString(): String = name
     }
+    case class Num(num: Int) extends Expr
 
     case class Load(arr: Expr, index: List[Expr]) extends Expr
     case class Plus(a: Expr, b: Expr) extends Expr 
     case class Times(a: Expr, b: Expr) extends Expr
     case class Assign(a: Expr, b: Expr) extends Expr
-    case class Range(a: Expr, b: Expr, step: Int) extends Expr
+    case class Range(a: Expr, b: Expr, step: Expr) extends Expr
     case class For(ite: Expr, range: Range, body: Expr) extends Expr 
     case class Stmt(s: List[Expr]) extends Expr
 
     case class Array(name: String) extends Expr
-    case class Matrix(name: String) extends Expr
-    case class CsrMat(data: Expr, rowPtr: Expr, colInd: Expr) extends Expr
+    case class Matrix(name: String) extends Expr {
+      override def toString(): String = s"Matrix ${name}"
+    }
+    case class CsrMat(name: String, data: Expr, rowPtr: Expr, colInd: Expr) extends Expr {
+      override def toString(): String = s"CsrMat ${name}"
+    }
   }
 
   object CodeGenerator {
@@ -28,6 +33,7 @@ object Spl2 {
 
       node match {
         case Ident(name) => name
+        case Num(num) => num.toString()
         case Plus(a, b) => {
           genCode(a, indent) + " + " + genCode(b, indent)
         }
@@ -35,7 +41,7 @@ object Spl2 {
           genCode(a, indent) + " * " + genCode(b, indent)
         }
         case Assign(a, b) => {
-          space + genCode(a, indent) + " = " + genCode(b, indent)
+          space + genCode(a, indent) + " = " + genCode(b, indent) + ";"
         }
         case Load(arr, i) => {
           val v = genCode(arr, indent)
@@ -45,7 +51,7 @@ object Spl2 {
         // case Range(st, end, step) =>
         case For(i, r, body) => {
           val vi = genCode(i, indent)
-          val v0 = s"for(${vi} = ${genCode(r.a, indent)}; ${vi} < ${genCode(r.b, indent)}; ${vi} += ${r.step}) {\n"
+          val v0 = s"for(${vi} = ${genCode(r.a, indent)}; ${vi} < ${genCode(r.b, indent)}; ${vi} += ${genCode(r.step, indent)}) {\n"
           val v1 = genCode(body, indent+2)
           space + v0 + v1 + space + s"\n${space}}" // + space + "}"
         }
@@ -65,19 +71,25 @@ object Spl2 {
   object Syntax {
     import Expr._
 
-    def ident(s: String): Expr = Ident(s)
-    def range(a: Expr, b: Expr, step: Int): Range = Range(a, b, step)
+    var cnt: Int = 0
+    val ident_list = ArrayBuffer[Ident]()
+
+    def ident(): Expr = {
+      val v = Ident("tmp" + cnt.toString)
+      ident_list.append(v)
+      cnt += 1
+      v
+    }
+    def range(a: Expr, b: Expr, step: Expr): Range = Range(a, b, step)
 
     def array(s: String) = Array(s)
     def matrix(s: String) = Matrix(s)
     def csrMat(s: String) = {
-      CsrMat(array(s"$s.data"), array(s"$s.rowPtr"), array(s"$s.colInd"))
+      CsrMat(s, array(s"$s.data"), array(s"$s.rowPtr"), array(s"$s.colInd"))
     }
 
-    // def update(a: Expr, b: Expr): Expr = Assign(a, b)
-
     implicit class IntOperations(n: Int) {
-      def int = Ident(n.toString())
+      def int = Num(n)
     }
 
     implicit class SyntaxOperations(a: Expr) {
@@ -93,16 +105,72 @@ object Spl2 {
         val out = Assign(a, acc)
         Stmt(List(init, l, out))
       }
+
+      def *(b: Expr) = {
+        (a, b) match {
+          case (Num(i), Num(j)) => Num(i * j)
+        }
+      }
+
+      def *(b: Int) = {
+        a match {
+          case Num(i) => Num(i * b)
+        }
+      }
     }
+    
+    implicit class ArrayOperations(a: Array) {
+      def apply(i: Expr) = Load(a, List(i))
+    }
+  
+    implicit class MatrixOperations(a: Matrix) {
+      def apply(i: Expr, j: Expr) = Load(a, List(i, j))
+    }
+
+    // data structure for holding a kernel function
+    class Func(name: String, root: Expr) {
+      var args = List[Expr]()
+
+      def genCode() = {
+        val arg_names = args.mkString(", ")
+        val func_str = s"void ${name}(${arg_names}) {\n"
+        val args_str = ident_list.map(x => s"  int ${x.toString()};").mkString("\n")
+        val body = CodeGenerator.genCode(root, 2)
+        val code = func_str + args_str + "\n" + body + "\n}"
+        code
+      }
+      def setArgs(a: List[Expr]) = {
+        args = a
+      }
+    }
+
+    // for the syntax to catch kernel name and body
+    case class kernel(name: String) {
+      def apply(root: Expr) = {
+        val k = new Func(name, root)
+        k
+      }
+    }
+  
+    case class loop(i: Expr, r: Range) {
+      def apply(body: Expr) = For(i, r, body)
+    }
+  }
+
+  object CsrSyntax {
+    import Expr._
+    import Syntax._ 
 
     implicit class CsrOperations(a: Expr) {
       def sumCsrRow(csr: CsrMat, b: Matrix, row: Expr, col: Expr) = {
-        val k = Ident("k")
+        // val k = Ident("k")
+        val k = ident()
         val row_str = row.toString()
         val end = Ident(row_str+"+1")
-        val r = range(Load(csr.rowPtr, List(row)), Load(csr.rowPtr, List(end)), 1)
+        val r = range(Load(csr.rowPtr, List(row)), Load(csr.rowPtr, List(end)), 1.int)
 
-        val acc = Ident("acc")
+        // val acc = Ident("acc")
+        val acc = ident()
         val init = Assign(acc, 0.int)
         val b_i = Load(csr.colInd, List(k))
 
@@ -151,10 +219,9 @@ object Spl2 {
         a match {
           case For(i, r, body) => {
             val new_s = r.step * factor
-            // val new_b0 = body.replaceIdent(i.toString(), s"${i.toString()}*$factor + t")
-            val new_b0 = body.replaceIdent("k", s"k*$factor + t")
-
-            val new_b1 = For(Ident("t"), range(0.int, factor.int, 1), new_b0)
+            val t = ident()
+            val new_b0 = body.replaceIdent(s"$i", s"$i*$factor + ${t}")
+            val new_b1 = For(t, range(0.int, factor.int, 1.int), new_b0)
             val f = For(i, range(r.a, r.b, new_s), new_b1)
             f
           }
@@ -166,51 +233,60 @@ object Spl2 {
         }
       }
     }
+  }
+}
 
-    implicit class ArrayOperations(a: Array) {
-      def apply(i: Expr) = Load(a, List(i))
-    }
-  
-    implicit class MatrixOperations(a: Matrix) {
-      def apply(i: Expr, j: Expr) = Load(a, List(i, j))
-    }
+// Spl example programs
+object SplPrograms {
+  import Spl2.Syntax._
+  import Spl2.CsrSyntax._
 
-    case class kernel(name: String) {
-      def apply(root: Expr) = {root}
+  def csr_spmm() = {
+    val A = csrMat("A")
+    val B = matrix("B")
+    val C = matrix("C")
+    
+    val k = kernel("csr_spmm_kernel") {
+      var i = ident()
+      var j = ident()
+      loop(i, range(1.int, 128.int, 1.int)) {
+        loop(j, range(1.int, 128.int, 1.int)) {
+          C(i, j).sumCsrRow(A, B, i, j)
+        }
+      }
     }
-  
-    case class loop(i: Expr, r: Range) {
-      def apply(body: Expr) = For(i, r, body)
+    k.setArgs(List(A, B, C))
+    k
+  }
+
+  def csr_spmm_tile() = {
+    val A = csrMat("A")
+    val B = matrix("B")
+    val C = matrix("C")
+    
+    val k = kernel("csr_spmm_kernel_tile") {
+      var i = ident()
+      var j = ident()
+      loop(i, range(1.int, 128.int, 1.int)) {
+        loop(j, range(1.int, 128.int, 1.int)) {
+          C(i, j).sumCsrRow(A, B, i, j).csrRowTile(32)
+        }
+      }
     }
+    k.setArgs(List(A, B, C))
+    k
   }
 }
 
 object TestSpl2 extends App {
-  import Spl2.Syntax._
-  import Spl2.CodeGenerator._
+  import SplPrograms._
 
-  val k = kernel("kernel") {
-    var i = ident("i")
-    var j = ident("j")
-    
-    var A = csrMat("A")
-    var B = matrix("B")
-    var C = matrix("C")
-    
-    loop(i, range(1.int, 128.int, 1)) {
-      loop(j, range(1.int, 128.int, 1)) {
-        C(i, j).sumCsrRow(A, B, i, j).csrRowTile(32)
-      }
-    }
-  } 
-  println(k)
-  
-  val func_str = "void CsrSpmmKernel(CsrMat A, Matrix B, Matrix C) {"
+  // choose one from here.
+  // (the code generator will have issues if two kernels coexist.)
+  val k = csr_spmm()
+  // val k = csr_spmm_tile()
 
-  // val code = genCode(k.root, 2)
   println("***** Generated Code *****")
   println()
-  println(func_str)
-  println(genCode(k, 2))
-  println("}")
+  println(k.genCode())
 }
